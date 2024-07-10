520bc2d5528895b82c93ea3e70b4afdaba53ad0d
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-09 23:52:22 -0300

    :construction: feat: adiciona páginas para notificações e ajustes

---
+++ frontend/src/pages/notificacoes.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/templete/Layout";
+
+export default function Home() {
+  return (
+    <Layout
+      titulo="Notificações"
+      subtitulo="Em construção!"
+    >
+      <h3>Aqui terão Notificações...</h3>
+    </Layout>
+  );
+}
