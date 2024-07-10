e16a1c6b4095365541f9595d53bf65a713c98da9
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-10 11:00:15 -0300

    :construction: feat: adiciona opção de sair ao menu lateral

--- frontend/src/components/templete/MenuItem.tsx
+++ frontend/src/components/templete/MenuItem.tsx
@@ -1,19 +1,33 @@
 import Link from "next/link"
 
 interface MenuItemProps {
-    url: string
+    url?: string
     texto: string
     icone: any
+    className?: string
+    onClick?: any
 }
 export default function MenuItem(props: MenuItemProps) {
+    function renderizarLink() {
+        return (
+            <div className={`flex flex-col justify-center items-center h-20 w-20`}>
+                {props.icone}
+                <span className={`text-xs font-ligh`}>{props.texto}</span>
+            </div>
+        )
+    }
+
     return (
-        <li className={`hover:bg-gray-100 flex justify-center flex-row`}>
-            <Link href={props.url}>
-                <div className={`flex flex-col justify-center items-center h-20 w-20`}>
-                    {props.icone}
-                    <span className={`text-xs font-light text-gray-600`}>{props.texto}</span>
-                </div>
-            </Link>
+        <li
+            className={`hover:bg-gray-100 text-gray-600 flex justify-center flex-row cursor-pointer ${props.className}`}
+            onClick={props.onClick}>
+            {props.url ? (
+                <Link href={props.url}>
+                    {renderizarLink()}
+                </Link>
+            ) : (
+                renderizarLink()
+            )}
         </li>
     )
 }
