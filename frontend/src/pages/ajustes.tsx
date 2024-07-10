0fd6a48c4f7c1e1dd272e1ca3c2e50d93ce722af
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-09 23:52:22 -0300

    :construction: feat: adiciona páginas para notificações e ajustes

---
+++ frontend/src/pages/ajustes.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/templete/Layout";
+
+export default function Ajustes() {
+  return (
+    <Layout
+      titulo="Ajustes"
+      subtitulo="Em construção!"
+    >
+      <h3>Aqui terão seus ajustes...</h3>
+    </Layout>
+  );
+}
