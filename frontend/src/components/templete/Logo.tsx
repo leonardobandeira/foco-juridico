9f69d7f39bfd590f4f948b1d67ad1436bb4f8b98
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-10 14:14:57 -0300

    :lipstick: feat: altera aparencia dos elementos para usar modo dark

--- frontend/src/components/templete/Logo.tsx
+++ frontend/src/components/templete/Logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import logo from '../../assets/logo/logo.svg';
+import logo from '../../assets/logo/logo-dark.svg';
 
 export default function Logo() {
     return (
--- frontend/src/components/templete/MenuLateral.tsx
+++ frontend/src/components/templete/MenuLateral.tsx
@@ -4,7 +4,7 @@ import MenuItem from "./MenuItem";
 
 export default function MenuLateral() {
     return (
-        <aside className={`flex flex-col`}>
+        <aside className={`flex flex-col dark:bg-gray-900 dark:text-gray-200`}>
             <div className={`
             h-40 w-40
             flex flex-col
@@ -21,7 +21,12 @@ export default function MenuLateral() {
                 <MenuItem url="./notificacoes" texto="Notificações" icone={IconeNotificacoes} />
             </ul>
             <ul className={``}>
-                <MenuItem onClick={() => {console.log("Sair!")} } texto="Sair" icone={IconeLogout} className="text-red-600 hover:bg-red-400 hover:text-white"/>
+                <MenuItem
+                    onClick={() => { console.log("Sair!") }}
+                    texto="Sair"
+                    icone={IconeLogout}
+                    className="text-red-600 hover:bg-red-400 hover:text-sky-50
+                    dark:text-red-600 dark:hover:text-sky-50" />
             </ul>
         </aside>
     )
