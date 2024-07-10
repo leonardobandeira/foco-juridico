3c2261c443b75cec7deb0d1223ace574e6988807
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-10 14:14:57 -0300

    :lipstick: feat: altera aparencia dos elementos para usar modo dark

--- frontend/src/components/templete/MenuItem.tsx
+++ frontend/src/components/templete/MenuItem.tsx
@@ -13,13 +13,13 @@ export default function MenuItem(props: MenuItemProps) {
             <div className={`flex flex-col justify-center items-center h-20 w-20`}>
                 {props.icone}
                 <span className={`text-xs font-ligh`}>{props.texto}</span>
-            </div>
+            </div >
         )
     }
 
     return (
         <li
-            className={`hover:bg-gray-100 text-gray-600 flex justify-center flex-row cursor-pointer ${props.className}`}
+            className={`hover:bg-gray-100 dark:hover:bg-gray-800 text-gray-600 dark:text-sky-50 flex justify-center flex-row cursor-pointer ${props.className}`}
             onClick={props.onClick}>
             {props.url ? (
                 <Link href={props.url}>
