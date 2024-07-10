de39d0d73b2a958c9e77d9c38df3fe75d1def6ee
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-10 16:49:42 -0300

    :lipstick: feat: cria uso do modo dark na aplicação

--- frontend/src/components/templete/Logo.tsx
+++ frontend/src/components/templete/Logo.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
-import logo from '../../assets/logo/logo-dark.svg';
+import logo from '../../assets/logo/logo.svg';
+import logo_dark from '../../assets/logo/logo-dark.svg';
+import useAppData from "../../data/hook/useAppData";
 
 export default function Logo() {
+    const { tema } = useAppData()
     return (
         <div className={`
             h-30 w-30
         `}>
-            <Image src={logo} alt="Logo"></Image>
+            <Image src={!tema ? logo : logo_dark} alt="Logo"></Image>
         </div>
     )
 }
