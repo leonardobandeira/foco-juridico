cfe499420d3a5c308d188646dc0a464a1af97e6a
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-10 00:23:50 -0300

    :lipstick: feat: ajusta elementos de menu para ficarem mais harmoniosos

--- frontend/src/components/templete/MenuItem.tsx
+++ frontend/src/components/templete/MenuItem.tsx
@@ -7,7 +7,7 @@ interface MenuItemProps {
 }
 export default function MenuItem(props: MenuItemProps) {
     return (
-        <li className={`hover:bg-gray-100`}>
+        <li className={`hover:bg-gray-100 flex justify-center flex-row`}>
             <Link href={props.url}>
                 <div className={`flex flex-col justify-center items-center h-20 w-20`}>
                     {props.icone}
