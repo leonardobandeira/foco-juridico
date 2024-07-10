4c24f5570aaba6b0e98f00f3c64cf5bbbd433d68
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-10 00:18:46 -0300

    :construction: feat: adiciona logo ao componente de menu lateral

--- frontend/src/components/templete/MenuLateral.tsx
+++ frontend/src/components/templete/MenuLateral.tsx
@@ -1,9 +1,20 @@
 import { IconeAjustes, IconeCasa, IconeNotificacoes } from "../icons";
+import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 export default function MenuLateral() {
     return (
         <aside>
+            <div className={`
+            h-40 w-40
+            flex flex-col
+            justify-center
+            items-center
+            p-2
+            `}>
+                <Logo />
+            </div>
+
             <ul>
                 <MenuItem url="./" texto="Início" icone={IconeCasa} />
             </ul>
