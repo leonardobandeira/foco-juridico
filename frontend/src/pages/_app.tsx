86229281ff4d11b8f6f6a910dea7423f9ba62ae3
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-10 14:33:40 -0300

    :construction: feat: adiciona appcontext a aplicação

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,8 @@
- import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css'
+import { AppProvider } from '../data/context/AppContext';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <AppProvider>
+    <Component {...pageProps} />
+  </AppProvider>;
 }
