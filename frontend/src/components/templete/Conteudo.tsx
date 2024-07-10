a03f0ead897afcee2929191b519bf8f1ee0023cc
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-09 22:48:33 -0300

    :construction: feat: adiciona layout base no projeto

---
+++ frontend/src/components/templete/Conteudo.tsx
@@ -0,0 +1,11 @@
+interface ConteudoProps {
+    children?: any
+}
+
+export default function Conteudo(props: ConteudoProps) {
+    return (
+        <div className={`flex flex-col mt-6`}>
+            {props.children}
+        </div>
+    )
+}
