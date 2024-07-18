976b891ebc967de6ed222fb316d8862b28ef31ed
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-18 12:50:38 -0300

    :spades: feat: adiciona icones ao projeto, iniciando pela tela de login

--- frontend/src/components/form/Botao.tsx
+++ frontend/src/components/form/Botao.tsx
@@ -8,7 +8,7 @@ interface BotaoProps {
 }
 
 export default function Botao({ texto, tipo = 'primario', onClick, className }: BotaoProps) {
-    const defaultClasses = 'w-full rounded-lg px-4 py-3 mt-6';
+    const defaultClasses = `w-full rounded-lg px-4 py-3 mt-6 font-semibold`;
     const tipoClasses = tipo === 'primario'
         ? 'bg-base1 hover:bg-blue-500 text-white'
         : 'bg-gray-900 hover:bg-gray-500 text-black';
