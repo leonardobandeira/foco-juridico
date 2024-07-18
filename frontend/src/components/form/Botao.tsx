fd2965daa46e5c36b2909f9baf10afa74ffebb08
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-17 22:30:33 -0300

    :construction: cria botão para usar no projeto

---
+++ frontend/src/components/form/Botao.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface BotaoProps {
+    texto: string;
+    tipo?: 'primario' | 'secundario';
+    onClick: () => void;
+    className?: string;
+}
+
+export default function Botao({ texto, tipo = 'primario', onClick, className }: BotaoProps) {
+    const defaultClasses = 'w-full rounded-lg px-4 py-3 mt-6';
+    const tipoClasses = tipo === 'primario'
+        ? 'bg-blue-900 hover:bg-blue-500 text-white'
+        : 'bg-gray-900 hover:bg-gray-500 text-black';
+
+    return (
+        <button onClick={onClick} className={`${defaultClasses} ${tipoClasses} ${className}`}>
+            {texto}
+        </button>
+    );
+}
