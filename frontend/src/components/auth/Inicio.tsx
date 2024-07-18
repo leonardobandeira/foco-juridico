8790e7bcd8d42b18aa7bb6b908da9daa3a5207cf
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-18 14:25:25 -0300

    :sparkle: feat: cria tela de "Login" ou "Solicitar acesso"

---
+++ frontend/src/components/auth/Inicio.tsx
@@ -0,0 +1,27 @@
+import Formulario from "../form/Formulario";
+import Botao from "../form/Botao";
+import Banner from "./Banner";
+import Rodape from "../templete/Rodape";
+
+export default function Inicio() {
+    return (
+        <div className="md:w-2/3 w-full flex flex-col justify-between gap-y-24">
+            <Banner />
+            <Formulario>
+                <Botao
+                    texto="Login"
+                    className="mt-10"
+                    tipo="primario"
+                    onClick={() => { console.log('Login'); }}
+                />
+                <Botao
+                    texto="Solicitar acesso"
+                    className="mt-10"
+                    tipo="secundario"
+                    onClick={() => { console.log('Solicitar acesso'); }}
+                />
+            </Formulario>
+            <Rodape />
+        </div>
+    );
+}
