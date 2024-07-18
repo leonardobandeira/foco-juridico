eaca935533b4c69c0fbaeb470f019ca60f867cfb
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-18 12:11:32 -0300

    :recycle: refactor: ajusta elementos visuais para otimizar uso de classes

--- frontend/src/pages/autenticacao.tsx
+++ frontend/src/pages/autenticacao.tsx
@@ -1,22 +1,21 @@
 import Banner from "../components/auth/Banner";
-import Rodape from "./../components/templete/Rodape";
+import Rodape from "../components/templete/Rodape";
 import Login from "../components/auth/Login";
 
 export default function Autenticacao() {
     return (
-        <div className={`h-screen flex flex-col justify-around`}>
-            <div className={`flex flex-col md:flex-row justify-center items-center`}>
-                <div className={`hidden md:inline-flex h-screen w-3/5 flex-row justify-center`}>
+        <div className="min-h-screen flex flex-col">
+            <div className="flex-grow flex flex-col md:flex-row justify-center items-center">
+                <div className="hidden md:flex h-screen w-3/5 justify-center">
                     <Banner />
                 </div>
-                
-                <div className={`md:w-2/5 w-full md:px-0 px-8
-                md:inline-flex flex-row justify-center items-center`}>
+                <div className="md:w-2/5 w-full md:px-0 px-8 flex justify-center items-center">
                     <Login />
                 </div>
-
-            </div >
-            <Rodape />
-        </div >
+            </div>
+            <div className="mt-auto">
+                <Rodape />
+            </div>
+        </div>
     )
 }
