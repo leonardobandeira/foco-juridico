6640d63450ed10f91347824cab838ecee827f2e9
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-19 01:19:55 -0300

    :sparkle: feat: cria página de registro

--- frontend/src/components/form/Botao.tsx
+++ frontend/src/components/form/Botao.tsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 
 interface BotaoProps {
-    texto: string;
-    tipo?: 'primario' | 'secundario';
+    texto?: string;
+    tipo?: 'primario' | 'secundario' | 'voltar';
     onClick: () => void;
     className?: string;
 }
 
 export default function Botao({ texto, tipo = 'primario', onClick, className }: BotaoProps) {
-    const defaultClasses = `w-full rounded-lg px-4 py-3 mt-6 font-semibold`;
-    const tipoClasses = tipo === 'primario'
-        ? 'bg-base1 hover:bg-blue-500 text-white'
-        : 'bg-base2 hover:bg-gray-500 text-white';
+    const defaultClasses = `w-full rounded-xl font-semibold`;
+    let tipoClasses = ''
+
+    if (tipo === 'primario') {
+        tipoClasses = 'bg-base1 hover:bg-blue-500 text-white px-4 py-3 mt-6 '
+    }
+    if (tipo === 'secundario') {
+        tipoClasses = 'bg-base2 hover:bg-gray-500 text-white px-4 py-3 mt-6 '
+    }
+    if (tipo === 'voltar') {
+        tipoClasses = 'text-left rounded-full'
+    }
 
     return (
         <button onClick={onClick} className={`${defaultClasses} ${tipoClasses} ${className}`}>
-            {texto}
+            {tipo === 'voltar' && <FontAwesomeIcon className="md:text-5xl text-3xl ml-4" icon={faAngleLeft} />}
+            {tipo !== 'voltar' && texto}
         </button>
     );
 }
