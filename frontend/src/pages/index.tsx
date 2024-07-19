ba9c2a5fcce370c2daa0f7d23a3fb0191c7d10c4
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-19 01:53:39 -0300

    :construction:  feat: inicia a construção da tela principal

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,22 @@
+import Banner from "../components/auth/Banner";
+import AlertasAtivos from "../components/listas/AlertasAtivos";
 import Layout from "../components/templete/Layout";
+import Quadrantes from "../components/templete/Quadrantes";
 
 export default function Home() {
+  let ladoEsquerdo = <Banner />;
+  let ladoDireito = <Banner />;
+  let rodape = null;
+
   return (
     <Layout
       titulo="Foco Jurídico"
       subtitulo="Em construção!"
     >
-      <h3>Página inicial...</h3>
+        <div className={`flex flex-row w-full flex-grow`}>
+        <AlertasAtivos/>
+        <AlertasAtivos/>
+        </div>
     </Layout>
   );
 }
