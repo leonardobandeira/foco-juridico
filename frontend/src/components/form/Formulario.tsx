d77fb24a35aff5f839687f3bb7af46c8a43adca6
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-19 00:35:07 -0300

    :lipstick: style: ajusta tamanho do texto

--- frontend/src/components/form/Formulario.tsx
+++ frontend/src/components/form/Formulario.tsx
@@ -1,8 +1,8 @@
 export default function Formulario(props) {
     return (
         <form className={`mb-6`}>
-            <h1 className={`font-bold text-2xl md:text-3xl mb-6 tracking-wider text-left w-full`}>
-               {props.titulo}
+            <h1 className={`font-bold text-4xl md:text-3xl mb-6 tracking-wider text-left w-full`}>
+                {props.titulo}
             </h1>
             {props.children}
         </form>
