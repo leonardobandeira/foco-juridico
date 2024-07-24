0f952569f26c67142a996b24df85b269da9f9bbf
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-24 15:19:06 -0300

    :construction: feat: ajusta mudança de contexto entre a página de login e registro
    Algumas páginas e botões foram ocultos pois não precisam fazer parte do fluxo, até agora.

--- frontend/src/components/auth/LinkInformativo.tsx
+++ frontend/src/components/auth/LinkInformativo.tsx
@@ -1,15 +1,18 @@
-import Image from "next/image";
-import banner from '../../assets/banner/banner.svg';
+import useAppData from "../../data/hook/useAppData";
+import { Auth, Tema } from './../../data/context/types'
 
 interface LinkInformativo {
     texto: string
     texto2: string
-    rota: string
+    rota: Auth
 }
 
 export default function LinkInformativo(props: LinkInformativo) {
+    const { setAuthPage } = useAppData();
+
     return (
-        <label className="text-gray-400 font-normal tracking-wider text-center">
+        <label onClick={() => { setAuthPage(props.rota) }}
+            className="text-gray-400 font-normal tracking-wider text-center">
             {props.texto} <span className="text-base1 font-bold">{props.texto2}</span>
         </label>
     )
--- frontend/src/pages/autenticacao.tsx
+++ frontend/src/pages/autenticacao.tsx
@@ -1,11 +1,8 @@
+import useAppData from "../data/hook/useAppData";
 import Banner from "../components/auth/Banner";
 import Login from "../components/auth/Login";
-import useAppData from "../data/hook/useAppData";
-import SolicitarAcesso from "../components/auth/SolicitarAcesso";
-import Quadrantes from "../components/templete/Quadrantes";
-import Sobre from "../components/sobre/Sobre";
 import Registrar from "../components/auth/Registrar";
-import Inicio from "../components/auth/Inicio";
+import Quadrantes from "../components/templete/Quadrantes";
 
 export default function Autenticacao() {
     const { authPage } = useAppData();
@@ -14,21 +11,12 @@ export default function Autenticacao() {
     let ladoDireito: JSX.Element | null = null;
     let rodape: JSX.Element | null = null;
 
-    if (authPage === '') {
-        ladoDireito = <Inicio />;
-        ladoEsquerdo = null;
-    }
-
     if (authPage === 'login') {
         ladoDireito = <Login />;
     }
 
     if (authPage === 'registrar') {
         ladoDireito = <Registrar />;
-    }
-
-    if (authPage === 'solicitar') {
-        ladoDireito = <SolicitarAcesso />;
     }
 
     return (
