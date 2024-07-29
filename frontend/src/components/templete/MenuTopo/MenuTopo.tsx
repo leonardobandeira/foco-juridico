5b641bd0f762d1b2dc64fff2e7edfe8634f8d9ac
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-29 15:30:23 -0300

    :recycle: fix: remove biblioteca não utilizada

--- frontend/src/components/templete/MenuTopo/MenuTopo.tsx
+++ frontend/src/components/templete/MenuTopo/MenuTopo.tsx
@@ -1,8 +1,6 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCog } from '@fortawesome/free-solid-svg-icons'
 import Foto from "./Foto";
 import Logo from "../Logo";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
 
 interface MenuTopoProps {
 
@@ -11,25 +9,16 @@ export default function MenuTopo(props: MenuTopoProps) {
     return (
         <div className={`w-full flex flex-row justify-between items-center mb-4 bg-gray-100 p-4`}>
             <Logo classes={`w-[120px]`} />
-            <DropdownMenu>
-                <DropdownMenuTrigger>
-                    <div className="flex gap-2 items-center">
-                        <div className="flex flex-col items-end">
-                            <span className="text-lg font-bold leading-5">Leonardo Bandeira</span>
-                            <span className="text-xs text-zinc-400">lucena.lbd@gmail.com</span>
-                        </div>
-                        <div className="flex justify-center items-center rounded-full overflow-hidden w-10 h-10 p-1 bg-zinc-700">
-                            <Foto classes={`rounded-full w-[50px] h-[50px]`} />
+            <div className="flex gap-2 items-center">
+                <div className="flex flex-col items-end">
+                    <span className="text-lg font-bold leading-5">Leonardo Bandeira</span>
+                    <span className="text-xs text-zinc-400">lucena.lbd@gmail.com</span>
+                </div>
+                <div className="flex justify-center items-center rounded-full overflow-hidden w-10 h-10 p-1 bg-zinc-700">
+                    <Foto classes={`rounded-full w-[50px] h-[50px]`} />
 
-                        </div>
-                    </div>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                    <DropdownMenuLabel>Menu Usuário</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem >Logout</DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
+                </div>
+            </div>
         </div>
     )
 }
