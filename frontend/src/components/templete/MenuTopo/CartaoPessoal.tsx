d1526cda4a048e99bb7972838960d72945956990
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-31 23:06:19 -0300

    :sparkle: feat: cria menu dropdown personalizado

---
+++ frontend/src/components/templete/MenuTopo/CartaoPessoal.tsx
@@ -0,0 +1,37 @@
+import { ReactNode, useState } from "react";
+import Foto from "./Foto";
+import ItemMenu from "@/components/templete/MenuTopo/ItemMenu";
+
+interface DropDownProps {
+    children: ReactNode;
+}
+
+export default function CartaoPessoal({ children }: DropDownProps) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsOpen(prev => !prev);
+    };
+
+    return (
+        <div className="relative cursor-pointer" onClick={toggleDropdown}>
+            <div className="flex items-center rounded px-3 py-1 text-sm text-gray-800 placeholder-gray-300 focus:outline-none">
+                <div className="flex gap-3 items-center">
+                    <div className="flex flex-col items-end">
+                        <span className="text-lg font-bold leading-5">Leonardo Bandeira</span>
+                        <span className="text-xs text-zinc-400">lucena.lbd@gmail.com</span>
+                    </div>
+                    <div className="flex justify-center items-center rounded-full overflow-hidden w-10 h-10 bg-zinc-700">
+                        <Foto classes="rounded-full w-10 h-10 object-cover" />
+                    </div>
+                </div>
+            </div>
+
+            {isOpen && (
+                <div className="absolute bg-white border border-gray-200 shadow-md rounded-xl w-[200px] max-w-full overflow-hidden mt-2 right-0">
+                    {children}
+                </div>
+            )}
+        </div>
+    );
+}
--- frontend/src/components/templete/MenuTopo/MenuTopo.tsx
+++ frontend/src/components/templete/MenuTopo/MenuTopo.tsx
@@ -1,6 +1,7 @@
-import Foto from "./Foto";
 import Logo from "../Logo";
-import MenuPessoal from './MenuPessoal';
+import CartaoPessoal from "./CartaoPessoal";
+import ItemMenu from "./ItemMenu";
+import { AlertCircle, User, Copyright, LogOut, Settings } from "lucide-react"
 
 interface MenuTopoProps {
 
@@ -9,14 +10,23 @@ export default function MenuTopo(props: MenuTopoProps) {
     return (
         <div className={`w-full flex flex-row justify-between items-center mb-4 bg-gray-100 p-4`}>
             <Logo classes={`w-[120px]`} />
-            
-            <MenuPessoal/>
+            <CartaoPessoal>
+                <span className={`flex items-center text-black font-semibold flex justify-center p-1.5 text-lg bg-gray-50 border-b border-base2`}>
+                    <Settings className="mr-2 h-5 w-5" />Minha Conta
+                </span>
+                <ItemMenu path="/">
+                    <AlertCircle className="mr-2 h-4 w-4" />Criar Alerta
+                </ItemMenu>
+                <ItemMenu path="/">
+                    <User className="mr-2 h-4 w-4" />Meus dados
+                </ItemMenu>
+                <ItemMenu path="/">
+                    <Copyright className="mr-2 h-4 w-4" />Sobre nós
+                </ItemMenu>
+                <ItemMenu path="/" style="text-red-500">
+                    <LogOut className="mr-2 h-4 w-4" />Sair
+                </ItemMenu>
+            </CartaoPessoal>
         </div>
     )
 }
-
-/***
-
-<BotaoAlternarTema tema={tema} alternarTema={alternarTema} />
-
- */
