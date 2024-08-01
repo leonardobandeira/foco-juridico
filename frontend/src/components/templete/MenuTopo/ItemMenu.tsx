06aa6a82bdbbc005371d74e204ed56c99e9aded7
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-31 23:06:19 -0300

    :sparkle: feat: cria menu dropdown personalizado

---
+++ frontend/src/components/templete/MenuTopo/ItemMenu.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+
+interface ItemMenuProps {
+    path: string;
+    style?: string
+    children: ReactNode;
+}
+
+export default function ItemMenu({ path = '', style = '', children }: ItemMenuProps) {
+    return (
+        <Link href={path}
+            className={`flex items-center w-[200px] text-lg text-black border-b border-gray-50 px-3 py-2 bg-white
+             hover:bg-gray-100 hover:font-semibold hover:text-xl
+            ${style !== '' ? (style + ' hover:bg-red-100') : ''}`}
+        >
+            {children}
+        </Link>
+    );
+}
