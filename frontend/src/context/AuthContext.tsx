291b46ddf1360575e1326bdac299ec568ef36299
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-08-04 23:26:09 -0300

    :construction: feat: cria provide para controlar o login

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,1 +1,39 @@
+import { createContext } from 'vm'
 import firebase from '../firebase/config'
+import Usuario from '@/model/Usuario'
+import { useState } from 'react'
+
+interface AuthContextProps {
+    usuario: Usuario
+    loginGoogle: () => Promise<void>
+}
+
+const AuthContext = createContext<AuthContextProps>({})
+
+async function usuarioNormalizado(usuarioFirebase: firebase.User): Promise<Usuario> {
+    const token = await usuarioFirebase.getIdToken()
+    return {
+        uid: usuarioFirebase.uid,
+        nome: usuarioFirebase.displayName,
+        email: usuarioFirebase.email,
+        token,
+        provedor: usuarioFirebase.providerData[0]?.providerId,
+        imagemUrl: usuarioFirebase.photoURL
+    }
+}
+
+export function AuthProvider(props) {
+    const [usuario, setUsuario] = useState<Usuario>(null)
+
+    async function loginGoogle() { }
+    return (
+        <AuthContext.Provider value={{
+            usuario,
+            loginGoogle
+        }}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContext
