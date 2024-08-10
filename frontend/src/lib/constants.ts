3bbd51a8b40307a0dfeefcb398f70e1624eb4f13
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-08-10 11:24:44 -0300

    :construction: feat: adiciona saída ao botão
    Adiciona o comportamento de saída ao botão de sair do menu superior.

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const ROOT_ROUTE = '/';
 export const HOME_ROUTE = '/admin';
 
-export const SESSION_COOKIE_NAME = 'user_session';
+export const SESSION_COOKIE_NAME = 'admin-foco-juridico-auth';
