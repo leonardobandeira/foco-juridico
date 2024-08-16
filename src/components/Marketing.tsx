da362ec0d6b6c8f5759b94f8034589a6da7add06
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-08-16 06:08:57 -0300

    :lipstick: style: adiciona possibilidade de style

--- src/components/Marketing.tsx
+++ src/components/Marketing.tsx
@@ -2,9 +2,13 @@ import TRF5 from './../assets/logo/trf5.svg'
 import Metropole from './../assets/logo/metropole.svg'
 import Image from 'next/image'
 
-export default function Marketing() {
+interface MarketingProps {
+    style?: string
+}
+
+export default function Marketing({ style }: MarketingProps) {
     return (
-        <div className={`h-1 flex flex-col gap-2`}>
+        <div className={`h-1 flex flex-col gap-2 ${style}`}>
             <p className={`font-medium text-pretty text-md text-center tracking-wider`}>Impulsionado por:</p>
             <div className={`flex flex-row justify-center gap-16`}>
                 <Image className={`w-20`} src={TRF5} alt="Logo do TRF5"></Image>
