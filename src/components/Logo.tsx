fbeceeea9562ec1dabc779db4286a52a68052ca2
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-08-21 22:18:34 -0300

    :construction: feat: redireciona o usuário para home

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,15 +1,27 @@
 "use client";
-
 import Image from "next/image";
 import logo from '../assets/logo/logo.svg';
 import logo_dark from '../assets/logo/logo-dark.svg';
 import useAppData from "@/data/hook/useAppData";
+import Link from "next/link";
 
 export default function Logo({ classes = '' }) {
-    const { tema } = useAppData()
-    return (
+    const { tema, usuario } = useAppData();
+
+    const renderLogo = () => (
         <div className={`${classes}`}>
             <Image src={!tema ? logo : logo_dark} alt="Logo"></Image>
         </div>
     )
+
+    const renderLogoLink = () => (
+        <Link href={'/home'}>
+            <div className={`${classes}`}>
+                <Image src={!tema ? logo : logo_dark} alt="Logo"></Image>
+            </div>
+        </Link>
+    )
+
+    return !usuario.uid ? renderLogo() : renderLogoLink()
+
 }
